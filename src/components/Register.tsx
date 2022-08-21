b8ad2572390596f819fce385e6a0c846d08e9e76
DESKTOP-HPJT45M\Steve <steverova0594@gmail.com>
2022-08-20 18:47:23 -0600

    update

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -29,60 +29,77 @@ function RegisterForm() {
         <React.Fragment>
             <div id='register-body'>
                 <div className="container pt-5 pb-5">
-                    <div className="col-md-8 col-lg-5 mx-auto">
+                    <div className="col-md-8 col-lg-8 mx-auto">
                         <div className="card ">
                             <h5 id="card-header" className="card-header">Agregar Persona</h5>
                             <div className="card-body">
                                 <form onSubmit={handleSubmit(onSubmit)}>
+
+                                    <div className="d-flex">
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <div className="form-group">
+                                                    <label htmlFor="">Nombre</label>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Ingrese su nombre"
+                                                        {...register("name", { required: true })}
+                                                    />
+                                                    {errors.password && <span className="text-danger">Este campo es requerido*</span>}
+                                                </div >
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="form-group ml-3">
+                                                    <label htmlFor="">Apelllido</label>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Ingrese su apellido"
+                                                        {...register("lastname", { required: true })}
+                                                    />
+                                                    {errors.password_confirm && <span className="text-danger">Este campo es requerido*</span>}
+                                                </div >
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <div className="form-group">
-                                        <label htmlFor="">Nombre</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Ingrese su nombre"
-                                            {...register("name", { required: true, pattern: /^[A-Za-z]+$/i } )}
-                                        />
-                                        {errors.name && <span className="text-danger">Este campo es requerido*</span>}
-                                    </div>
-
-                                    <div className="form-group mt-3">
-                                        <label htmlFor="">Apellido</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Ingrese su apellido"
-                                            {...register("lastname", { required: true })}
-                                        />
-                                        {errors.lastname && <span className="text-danger">Este campo es requerido*</span>}
-                                    </div>
-
-                                    <div className="form-group mt-3">
                                         <label htmlFor="">Correo</label>
                                         <input
                                             type="text"
-                                            placeholder="Ingrese su apellido"
+                                            placeholder="Correo: example@domian.com"
                                             {...register("email", { required: true })}
                                         />
                                         {errors.email && <span className="text-danger">Este campo es requerido*</span>}
                                     </div>
 
-                                    <div className="form-group mt-3 ">
-                                        <label htmlFor="">Contraseña</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Ingrese su contraseña"
-                                            {...register("password", { required: true, maxLength: 8 })}
-                                        />
-                                        {errors.password && <span className="text-danger">Este campo es requerido*</span>}
-                                    </div >
+                                    <div className="d-flex">
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <div className="form-group">
+                                                    <label htmlFor="">Contraseña</label>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Ingrese su contraseña"
+                                                        {...register("password", { required: true, maxLength: 8 })}
+                                                    />
+                                                    {errors.password && <span className="text-danger">Este campo es requerido*</span>}
+                                                </div >
+                                            </div>
 
-                                    <div className="form-group mt-3 ">
-                                        <label htmlFor="">Confirmar Contraseña</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Confirme su contraseña"
-                                            {...register("password_confirm", { required: true, maxLength: 8 })}
-                                        />
-                                        {errors.password_confirm && <span className="text-danger">Este campo es requerido*</span>}
-                                    </div >
+                                            <div className="col-6">
+                                                <div className="form-group ml-3">
+                                                    <label htmlFor="">Contraseña*</label>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Confirme su contraseña"
+                                                        {...register("password_confirm", { required: true, maxLength: 8 })}
+                                                    />
+                                                    {errors.password_confirm 
+                                                    && <span className="text-danger">Este campo es requerido*</span> }
+                                                </div >
+                                            </div >
+                                        </div >
+                                    </div>
 
                                     <button className="btn btn-secondary d-flex justify-content-center align-content-between" type="submit">
                                         <i className="material-symbols-sharp mr-1">
