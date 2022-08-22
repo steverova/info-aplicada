e6a63be36fd111fe8bc381c7db704e9ab39e5a00
DESKTOP-HPJT45M\Steve <steverova0594@gmail.com>
2022-08-21 19:46:19 -0600

    forms validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,65 +1,93 @@
 import "../public/css/style.css";
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
+  const [data, setData] = useState("");
+  const onSubmit: SubmitHandler<LoginForm> = (data) =>
+    setData(JSON.stringify(data));
+  return (
+    <React.Fragment>
+      <div id="register-body">
+        <div className="container pt-5">
+          <div className="col-md-8 col-lg-5 mx-auto">
+            <div className="card ">
+              <h5 id="card-header" className="card-header">
+                Iniciar Sesion
+              </h5>
+              <div className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="form-group">
+                    <label htmlFor="">Correo</label>
+                    <input
+                      type="text"
+                      {...register("email", { required: true })}
+                      style={{
+                        border: errors.email ? "2px solid red" : "",
+                      }}
+                      placeholder={
+                        errors.email
+                          ? "Este campo es requerido*"
+                          : "Ingrese su nombre"
+                      }
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <label htmlFor="">Contraseña</label>
+                    <input
+                      type="text"
+                      {...register("password", { required: true })}
+                      style={{
+                        border: errors.password ? "2px solid red" : "",
+                      }}
+                      placeholder={
+                        errors.password
+                          ? "Este campo es requerido*"
+                          : "Ingrese su nombre"
+                      }
+                    />
+                  </div>
+                  <button
+                    className="btn btn-secondary d-flex justify-content-center align-content-between"
+                    type="submit"
+                  >
+                    <i className="material-symbols-outlined mr-1">login</i>
+                    Iniciar sesión
+                  </button>
+                  <Link to="/register">
+                    <small className="">
+                      <p className="mt-3">
+                        ¿No tiene cuenta? <strong>Registrese</strong>
+                      </p>
+                    </small>
+                  </Link>
 
-    return (
-        <React.Fragment>
-            <div id='register-body'>
-                <div className="container pt-5">
-                    <div className="col-md-8 col-lg-5 mx-auto">
-                        <div className="card ">
-                            <h5 id="card-header" className="card-header">Iniciar Sesion</h5>
-                            <div className="card-body">
-                                <form>
-                                    <div className="form-group">
-                                        <label htmlFor="">Correo</label>
-                                        <input
-                                            type="text"
-                                            name="nombre"
-                                            placeholder="Ingrese su correo"
-                                            required
-                                        />
-                                    </div>
-
-                                    <div className="form-group mt-3">
-                                        <label htmlFor="">Contraseña</label>
-                                        <input
-                                            type="text"
-                                            name="apellido"
-                                            placeholder="Ingrese su contraseña"
-                                            required
-                                        />
-                                    </div>
-
-                                    <button className="btn btn-secondary d-flex justify-content-center align-content-between" type="submit">
-                                        <i className="material-symbols-outlined mr-1">
-                                            login
-                                        </i>
-                                        Iniciar sesión
-                                    </button>
-
-                                    <hr />
-
-                                    <Link to="/register">
-                                    <small className="">
-                                        <p className="mt-3">¿No tiene cuenta? <strong>Registrese</strong></p>
-                                    </small>
-                                    </Link>
-
-                                    <small>
-                                        <p>Volver a <strong>Inicio</strong></p>
-                                    </small>
-                                </form >
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                  <small>
+                    <p>
+                      Volver a <strong>Inicio</strong>
+                    </p>
+                  </small>
+                </form>
+                <p>{data}</p>
+              </div>
             </div>
-        </React.Fragment >
-    );
-
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Register;
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,127 +1,166 @@
 import "../public/css/style.css";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useState } from "react";
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type UserForm = {
-
-    id: string
-    name: string
-    lastname: string
-    email: string
-    password: string
-    password_confirm: string
-
-}
+  id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
 
 function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserForm>();
+  const [data, setData] = useState("");
+  const onSubmit: SubmitHandler<UserForm> = (data) =>
+    setData(JSON.stringify(data));
 
-    const { register, handleSubmit, formState: { errors } } = useForm<UserForm>();
-    const [data, setData] = useState("");
-    const onSubmit: SubmitHandler<UserForm> = data => setData(JSON.stringify(data));;
+  // const onSubmit = handleSubmit((data) => {
+  //     setData(JSON.stringify(data));
+  //     console.log(JSON.stringify(data))
+  // });
 
-    // const onSubmit = handleSubmit((data) => {
-    //     setData(JSON.stringify(data));
-    //     console.log(JSON.stringify(data))
-    // });
-    return (
-        <React.Fragment>
-            <div id='register-body'>
-                <div className="container pt-5 pb-5">
-                    <div className="col-md-8 col-lg-8 mx-auto">
-                        <div className="card ">
-                            <h5 id="card-header" className="card-header">Agregar Persona</h5>
-                            <div className="card-body">
-                                <form onSubmit={handleSubmit(onSubmit)}>
-
-                                    <div className="d-flex">
-                                        <div className="row">
-                                            <div className="col-6">
-                                                <div className="form-group">
-                                                    <label htmlFor="">Nombre</label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Ingrese su nombre"
-                                                        {...register("name", { required: true })}
-                                                    />
-                                                    {errors.password && <span className="text-danger">Este campo es requerido*</span>}
-                                                </div >
-                                            </div>
-                                            <div className="col-6">
-                                                <div className="form-group ml-3">
-                                                    <label htmlFor="">Apelllido</label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Ingrese su apellido"
-                                                        {...register("lastname", { required: true })}
-                                                    />
-                                                    {errors.password_confirm && <span className="text-danger">Este campo es requerido*</span>}
-                                                </div >
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label htmlFor="">Correo</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Correo: example@domian.com"
-                                            {...register("email", { required: true })}
-                                        />
-                                        {errors.email && <span className="text-danger">Este campo es requerido*</span>}
-                                    </div>
-
-                                    <div className="d-flex">
-                                        <div className="row">
-                                            <div className="col-6">
-                                                <div className="form-group">
-                                                    <label htmlFor="">Contraseña</label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Ingrese su contraseña"
-                                                        {...register("password", { required: true, maxLength: 8 })}
-                                                    />
-                                                    {errors.password && <span className="text-danger">Este campo es requerido*</span>}
-                                                </div >
-                                            </div>
-
-                                            <div className="col-6">
-                                                <div className="form-group ml-3">
-                                                    <label htmlFor="">Contraseña*</label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Confirme su contraseña"
-                                                        {...register("password_confirm", { required: true, maxLength: 8 })}
-                                                    />
-                                                    {errors.password_confirm 
-                                                    && <span className="text-danger">Este campo es requerido*</span> }
-                                                </div >
-                                            </div >
-                                        </div >
-                                    </div>
-
-                                    <button className="btn btn-secondary d-flex justify-content-center align-content-between" type="submit">
-                                        <i className="material-symbols-sharp mr-1">
-                                            save
-                                        </i>
-                                        Guardar
-                                    </button>
-                                    <Link to="/login">
-                                        <small className="">
-                                            <p className="mt-3">¿Ya tiene una cuenta? <strong>Inicie sesion</strong></p>
-                                        </small>
-                                    </Link>
-                                </form >
-                                <p>{data}</p>
-                            </div>
+  return (
+    <React.Fragment>
+      <div id="register-body">
+        <div className="container pt-5 pb-5">
+          <div className="col-md-8 col-lg-8 mx-auto">
+            <div className="card ">
+              <h5 id="card-header" className="card-header">
+                Agregar Persona
+              </h5>
+              <div className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="d-flex">
+                    <div className="row">
+                      <div className="col-6">
+                        <div className="form-group">
+                          <label htmlFor="">Nombre</label>
+                          <input
+                            type="text"
+                            {...register("name", { required: true })}
+                            style={{
+                              border: errors.name ? "2px solid red" : "",
+                            }}
+                            placeholder={
+                              errors.name
+                                ? "Este campo es requerido*"
+                                : "Ingrese su nombre"
+                            }
+                          />
                         </div>
+                      </div>
+                      <div className="col-6">
+                        <div className="form-group ml-3">
+                          <label htmlFor="">Apelllido</label>
+                          <input
+                            type="text"
+                            {...register("lastname", { required: true })}
+                            style={{
+                              border: errors.lastname ? "2px solid red" : "",
+                            }}
+                            placeholder={
+                              errors.lastname
+                                ? "Este campo es requerido*"
+                                : "Ingrese su apellido"
+                            }
+                          />
+                        </div>
+                      </div>
                     </div>
-                </div>
-            </div>
-        </React.Fragment >
-    );
+                  </div>
 
+                  <div className="form-group">
+                    <label htmlFor="">Correo</label>
+                    <input
+                      type="text"
+                      {...register("email", { required: true })}
+                      style={{
+                        border: errors.email ? "2px solid red" : "",
+                      }}
+                      placeholder={
+                        errors.email
+                          ? "Este campo es requerido*"
+                          : "Correo: ejemplo@dominio.com"
+                      }
+                    />
+                  </div>
+
+                  <div className="d-flex">
+                    <div className="row">
+                      <div className="col-6">
+                        <div className="form-group">
+                          <label htmlFor="">Contraseña</label>
+                          <input
+                            type="text"
+                            {...register("password", { required: true })}
+                            style={{
+                              border: errors.password ? "2px solid red" : "",
+                            }}
+                            placeholder={
+                              errors.lastname
+                                ? "Este campo es requerido*"
+                                : "Ingrese su contraseña"
+                            }
+                          />
+                        </div>
+                      </div>
+
+                      <div className="col-6">
+                        <div className="form-group ml-3">
+                          <label htmlFor="">Contraseña*</label>
+                          <input
+                            type="text"
+                            {...register("password_confirm", {
+                              required: true,
+                            })}
+                            style={{
+                              border: errors.password_confirm
+                                ? "2px solid red"
+                                : "",
+                            }}
+                            placeholder={
+                              errors.password_confirm
+                                ? "Este campo es requerido*"
+                                : "Ingrese su contraseña"
+                            }
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <button
+                    className="btn btn-secondary d-flex justify-content-center align-content-between"
+                    type="submit"
+                  >
+                    <i className="material-symbols-sharp mr-1">save</i>
+                    Guardar
+                  </button>
+                  <a href={"/Login"}>
+                    <small>
+                      <p className="mt-3">
+                        ¿Ya tiene una cuenta? <strong>Inicie sesion</strong>
+                      </p>
+                    </small>
+                  </a>
+                </form>
+                <p>{data}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default RegisterForm;
